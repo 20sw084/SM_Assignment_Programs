556f8062ee6828286538e5857f1f17ac6db7ca6f
Junaid Aslam <junaidaslam.muet@gmail.com>
2024-11-12 22:41:27 +0500

    Update Q9.java

--- Q9.java
+++ Q9.java
@@ -4,44 +4,62 @@ public class Q9 {
     static final int N = 50;
 
     public static void main(String[] args) {
-        int na = 0, nd = 0;
-        double n = 0, ta = generateExponential(LAMBDA), td = Double.MAX_VALUE;
-        double totTi = 0, totTu = 0, totalWaitTime = 0;
+        double currentTime = 0; // Internal clock time
+        int numberOfArrivals = 0; // Count of arrivals
+        int numberOfDepartures = 0; // Count of departures
+        double nextArrivalTime = generateExponential(LAMBDA); // Time of the next arrival
+        double nextDepartureTime = Double.MAX_VALUE; // Time of the next departure (initially infinity)
+        double totalInterarrivalTime = 0; // Running total of interarrival times
+        double totalServiceTime = 0; // Running total of service times
+        double totalWaitTime = 0; // Total time spent waiting in the queue
 
-        while (nd < N) {
-            if (ta < td) {
-                // Arrival event
-                n = ta;
-                na++;
-                totTi += ta;
-                ta = n + generateExponential(LAMBDA);
-                if (nd == na - 1) {
-                    td = n + generateExponential(MU);
-                    totTu += td - n;
+        // Run the simulation until all N customers are served
+        while (numberOfDepartures < N) {
+            if (nextArrivalTime <= nextDepartureTime) {
+                // Process arrival event
+                currentTime = nextArrivalTime;
+                numberOfArrivals++;
+                totalInterarrivalTime += nextArrivalTime - (numberOfArrivals > 1 ? currentTime : 0);
+
+                // Schedule the next arrival
+                nextArrivalTime = currentTime + generateExponential(LAMBDA);
+
+                // If the server is idle, schedule a departure
+                if (numberOfArrivals - numberOfDepartures == 1) {
+                    double serviceTime = generateExponential(MU);
+                    totalServiceTime += serviceTime;
+                    nextDepartureTime = currentTime + serviceTime;
                 }
             } else {
-                // Departure event
-                n = td;
-                nd++;
-                if (na > nd) {
-                    td = n + generateExponential(MU);
-                    totTu += td - n;
-                    totalWaitTime += td - n;
+                // Process departure event
+                currentTime = nextDepartureTime;
+                numberOfDepartures++;
+
+                // Schedule the next departure if there are customers in the queue
+                if (numberOfArrivals > numberOfDepartures) {
+                    double serviceTime = generateExponential(MU);
+                    totalServiceTime += serviceTime;
+                    nextDepartureTime = currentTime + serviceTime;
                 } else {
-                    td = Double.MAX_VALUE;
+                    nextDepartureTime = Double.MAX_VALUE; // No more departures if the queue is empty
                 }
+
+                // Update the total wait time
+                totalWaitTime += currentTime - (numberOfDepartures <= 1 ? 0 : nextDepartureTime);
             }
         }
 
-        double meanInterarrival = totTi / na;
-        double meanService = totTu / nd;
-        double meanWait = totalWaitTime / nd;
+        // Calculate and display terminal statistics
+        double meanInterarrivalTime = totalInterarrivalTime / numberOfArrivals;
+        double meanServiceTime = totalServiceTime / numberOfDepartures;
+        double meanWaitTime = totalWaitTime / numberOfDepartures;
 
-        System.out.println("Mean Interarrival Time: " + meanInterarrival);
-        System.out.println("Mean Service Time: " + meanService);
-        System.out.println("Mean Waiting Time: " + meanWait);
+        System.out.println("Mean Interarrival Time: " + meanInterarrivalTime);
+        System.out.println("Mean Service Time: " + meanServiceTime);
+        System.out.println("Mean Time Spent in Queue: " + meanWaitTime);
     }
 
+    // Function to generate exponentially distributed random times
     public static double generateExponential(double rate) {
         return -Math.log(1 - Math.random()) / rate;
     }
